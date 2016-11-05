5dd3a9d83088c3eb6af3eaf1470f215f4429c388
Linh Nguyen <linh.nguyen@sbgenomics.com>
2016-11-05 11:37:33 -0400

    Add user priveleges

--- hello.py
+++ hello.py
@@ -15,3 +15,30 @@ logging.warning(json.dumps(inventory, sort_keys=True, indent=4))
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+# Show user profile and his/her inventory list
+@app.route('/user/<id>')
+def show_user_profile(id):
+	try:
+		uid = int(id)
+		return 'User %d' % uid
+	except ValueError:
+		return 'Some error occurred'
+
+# Show inventory profile
+@app.route('/inventory/<id>')
+def show_inventory_id(id):
+	try:
+		iid = int(id)
+		return 'Inventory %d' % iid
+	except ValueError:
+		return 'Some error occurred'
+
+# Create a reservation. Must be an approved user
+@app.route('/reserve')
+def make_reservation():
+	return 'Make a reservation'
+
+@app.route('/show_inventory')
+def show_inventory():
+	return 'Show inventory'
